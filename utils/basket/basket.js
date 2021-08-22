60ee40467cb2910de4161726a671fad687930e86
LewisChatham <lewis.chatham@outlook.com>
2021-08-22 12:14:23 +0100

    class: Basket method: Add item

--- utils/basket/basket.js
+++ utils/basket/basket.js
@@ -1,10 +1,17 @@
+const Item = require("../item")
+
 class Basket {
     constructor(userId) {
         this.userId = userId;
+        this.items = []
     }
 
     addItem(item) {
-
+        if (item instanceof Item) {
+            this.items.push(item)
+        } else {
+            throw new Error("Can only add instances of an Item to a basket")
+        }
     }
 }
 
--- utils/basket/basket.test.js
+++ utils/basket/basket.test.js
@@ -1,10 +1,40 @@
+const Item = require('../item')
 const Basket = require('./basket')
 
 describe("utils: Basket", () => {
-    it("Has a userId property which is a number", () => {
-        const lewisBasket = new Basket(1)
+    describe("Properties: ", () => {
+        it("Has a userId property which is a number", () => {
+            const lewisBasket = new Basket(1)
 
-        expect(lewisBasket.hasOwnProperty("userId")).toBeTruthy();
-        expect(typeof lewisBasket.userId).toBe("number");
-    }) 
+            expect(lewisBasket.hasOwnProperty("userId")).toBeTruthy();
+            expect(typeof lewisBasket.userId).toBe("number");
+        }) 
+    })
+    describe("Methods: ", () => {
+        describe("Add item: ", () => {
+            it("Allows me to add an item to my basket", () => {
+                const lewisBasket = new Basket(1);
+                const bread = new Item("Warburtons", 1, 2)
+
+                expect(lewisBasket.items.length).toBe(0)
+
+                lewisBasket.addItem(bread);
+
+                expect(lewisBasket.items.length).toBe(1)
+                expect(lewisBasket.items[0].name).toBe("Warburtons")
+            })
+            it("Throws an error if user tries to add a none item to basket", () => {
+                const lewisBasket = new Basket(1);
+                const nonItem = "blablablabla";
+
+                
+
+                expect(() => {
+                    lewisBasket.addItem(nonItem);
+                }).toThrowError("Can only add instances of an Item to a basket")
+            })
+        })
+    })
+
+    
 })
